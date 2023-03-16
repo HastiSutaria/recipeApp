0b2fbcf92b7dd9de35e63024ca68adf06c3d5a7d
HastiSutaria <hastisutaria.ict19@gmail.com>
2023-03-16 12:35:31 +0530

    dynamic components, module structures and optimization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey : 'AIzaSyAfFI4oG1Jm0-MoVg18qdSt0IET-JWvSxg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey : 'AIzaSyAfFI4oG1Jm0-MoVg18qdSt0IET-JWvSxg'
 };
 
 /*
